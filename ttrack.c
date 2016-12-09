15cb4d492355b744680e51e0d50c49bfd550723b
Sean Monroe <seanmonroe86@gmail.com>
2016-12-09 16:15:12 -0500

    altered entry usage, simplified arg storage

--- ttrack.c
+++ ttrack.c
@@ -3,40 +3,87 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int enter(int, char *, int, char *, char *);
+enum Flag {AFLAG, DFLAG, NFLAG, SFLAG, TFLAG, NUMFLAG};
+enum Val {AVAL, DVAL, NVAL, SVAL, TVAL, NUMVAL};
+
+int enter(int *, char *[64], char *);
 
 int main(int argc, char *argv[]) {
-	int nflag = 0;
-	int tflag = 0;
-	char *nval = NULL;
-	char *tval = NULL;
 	int index, c;
+	char command[64];
+	int flags[NUMFLAG];
+	char *vals[NUMVAL];
+
 	opterr = 0;
+	for (int i = 0; i < NUMFLAG; i++) flags[i] = 0;
+	for (int i = 0; i < NUMVAL; i++) vals[i] = NULL;
+
+	if (argc == 1) {
+		enter(flags, vals, "list");
+		return 0;
+	}
+	else optind = 2;
 
 	// Up to one of each option is permitted. They both require an argument.
 	// Failure at any point aborts with an error message.
-	while ((c = getopt(argc, argv, "n:t:")) != -1) {
+	while ((c = getopt(argc, argv, "a:d:n:s:t:")) != -1) {
 		switch (c) {
-			case 'n':
-				nflag++;
-				nval = optarg;
-				if (nflag > 1) {
+			case 'a':
+				vals[AVAL] = optarg;
+				flags[AFLAG]++;
+				if (flags[AFLAG] > 1) {
 					printf("Only one argument -%c allowed.\n", c);
 					return 1;
 				}
-				if (nval[0] == '-') {
+				if (vals[AVAL][0] == '-') {
+					printf("Option -%c requires an argument.\n", c);
+					return 1;
+				}
+				break;
+			case 'd':
+				vals[DVAL] = optarg;
+				flags[DFLAG]++;
+				if (flags[DFLAG] > 1) {
+					printf("Only one argument -%c allowed.\n", c);
+					return 1;
+				}
+				if (vals[DVAL][0] == '-') {
+					printf("Option -%c requires an argument.\n", c);
+					return 1;
+				}
+				break;
+			case 'n':
+				vals[NVAL] = optarg;
+				flags[NFLAG]++;
+				if (flags[NFLAG] > 1) {
+					printf("Only one argument -%c allowed.\n", c);
+					return 1;
+				}
+				if (vals[NVAL][0] == '-') {
+					printf("Option -%c requires an argument.\n", c);
+					return 1;
+				}
+				break;
+			case 's':
+				vals[SVAL] = optarg;
+				flags[SFLAG]++;
+				if (flags[SFLAG] > 1) {
+					printf("Only one argument -%c allowed.\n", c);
+					return 1;
+				}
+				if (vals[SVAL][0] == '-') {
 					printf("Option -%c requires an argument.\n", c);
 					return 1;
 				}
 				break;
 			case 't':
-				tflag++;
-				tval = optarg;
-				if (tflag > 1) {
+				vals[TVAL] = optarg;
+				flags[TFLAG]++;
+				if (flags[TFLAG] > 1) {
 					printf("Only one argument -%c allowed.\n", c);
 					return 1;
 				}
-				if (tval[0] == '-') {
+				if (vals[TVAL][0] == '-') {
 					printf("Option -%c requires an argument.\n", c);
 					return 1;
 				}
@@ -54,25 +101,24 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	// Count number of args after options, must be 1 or fail
-	c = 0;
-	for (index = optind; index < argc; index++) c++;
-	if (c != 1) {
-		printf("Usage: ttrack [-t TITLE] [-n NOTE] COMMAND");
+	// Count number of args after options, must be 0 or fail
+	if (optind < argc) {
+		printf("Usage: ttrack COMMAND [-OPTIONS ARG]");
 		return 1;
 	}
 
 	// Pass validated info to next step
-	enter(nflag, nval, tflag, tval, argv[optind]);
+	enter(flags, vals, argv[1]);
 
 	return 0;
 }
 
-
-int enter(int n, char *nval, int t, char *tval, char *command) {
+int enter(int *n, char *v[64], char *command) {
 	// Debug/verification print
-	printf("n = %d\nnval = %s\nt = %d\ntval = %s\ncommand = %s\n",
-			n, nval, t, tval, command);
+	for (int i = 0; i < NUMFLAG; i++) {
+		printf("arg %d = %d %s\n", i, n[i], v[i]);
+	}
+	printf("command = %s\n", command);
 
 	return 0;
 }
